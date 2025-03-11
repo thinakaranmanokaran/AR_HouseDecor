5abca024b9b7fd279a294d5e0ca01336ec12a8b5
Thinakaran <thinakaranmanohar@yahoo.com>
2025-03-11 14:23:55 +0530

    Iniiiii

---
+++ client/src/pages/HomeScreen.jsx
@@ -0,0 +1,13 @@
+// src/screens/HomeScreen.jsx
+import { View, Text } from 'react-native';
+// import { NativeWindStyleSheet } from 'nativewind';
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text className=' text-red-500 font-bold ' >Welcomess Hello to Home Screen</Text>
+        </View>
+    );
+};
+
+export default HomeScreen;
